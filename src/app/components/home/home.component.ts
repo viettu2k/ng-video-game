e05bd124efdd4e63a48d445d135bd758b91912e0
viettu2k <doanviettu2k@gmail.com>
2022-07-25 15:09:56 +0700

    implementing sort menu

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,27 +1,32 @@
 import { HttpService } from './../../services/http.service';
 import { Game, APIResponse } from './../../models';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public sort!: string;
   public games!: Game[];
+  private routeSub!: Subscription;
+  private gameSub!: Subscription;
+
   constructor(
     private httpService: HttpService,
+    private router: Router,
     private activatedRoute: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params) => {
+    this.routeSub = this.activatedRoute.params.subscribe((params) => {
       if (params['game-search']) {
-        this.searchGames('metacritic', params['game-search']);
+        this.searchGames('metacrit', params['game-search']);
       } else {
-        this.searchGames('metacritic');
+        this.searchGames('metacrit');
       }
     });
   }
@@ -31,6 +36,21 @@ export class HomeComponent implements OnInit {
       .getGameList(sort, search)
       .subscribe((gameList: APIResponse<Game>) => {
         this.games = gameList.results;
+        console.log(gameList);
       });
+  }
+
+  openGameDetails(id: string): void {
+    this.router.navigate(['details', id]);
+  }
+
+  ngOnDestroy(): void {
+    if (this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
   }
 }
